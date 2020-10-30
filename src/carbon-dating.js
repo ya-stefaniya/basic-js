add10df53c7814932afe6ff0b661a4065c8fc832
ya-stefaniya <62294117+ya-stefaniya@users.noreply.github.com>
2020-10-30 16:06:31 +0300

    add: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,10 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof(sampleActivity) != 'string') return false;
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  let period = Math.log2(MODERN_ACTIVITY / +sampleActivity) / k;
+  if (!isFinite(period) || period < 1) return false;
+  return Math.ceil(period);
 };
