95580a7994bb945560d42829a0df50b95bd8dd0b
ya-stefaniya <62294117+ya-stefaniya@users.noreply.github.com>
2020-10-30 16:07:53 +0300

    add: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+
+  if (!Array.isArray(members)) {
+    return false
+  };
+
+  let resultArr = members.filter(element => typeof (element) === 'string');
+  return resultArr.map(element => element.trim()[0].toLocaleUpperCase()).sort().join('') || false;
 };
